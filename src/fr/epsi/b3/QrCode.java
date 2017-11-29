ce4a9fcc16d299addd5905a478cfaead2c250a2d
Maxime Cohen <maxime.cohen-pro@outlook.fr>
2017-11-29 17:50:17 +0100

    update qrcode class

--- src/fr/epsi/b3/QrCode.java
+++ src/fr/epsi/b3/QrCode.java
@@ -1,12 +1,18 @@
 package fr.epsi.b3;
 
 import java.awt.Image;
-import java.awt.image.BufferedImage;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QrCode {
-
-	public static Image generate(String content) {
-		//return new BufferedImage();
-		return null;
+	
+	public static Image generate(String content, int size) throws WriterException {
+		QRCodeWriter qrWriter = new QRCodeWriter();
+		Object matrix = qrWriter.encode(content, BarcodeFormat.QR_CODE, size, size);
+		return MatrixToImageWriter.toBufferedImage((BitMatrix) matrix);
 	}
 }
